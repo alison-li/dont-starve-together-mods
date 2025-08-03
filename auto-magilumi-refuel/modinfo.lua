bc30e86d7c95bb5042c7f5fee48d473a0086911a
Alison Li <alison-li@github.com>
2025-08-03 20:25:09 +0000

    Clean up

--- auto-magilumi-refuel/modinfo.lua
+++ auto-magilumi-refuel/modinfo.lua
@@ -5,37 +5,39 @@ version = "1.0.0"
 
 api_version = 10
 dst_compatible = true
-client_only_mod = true  -- This is the key setting!
+client_only_mod = true
 all_clients_require_mod = false
 
 configuration_options = {
     {
         name = "refuel_threshold",
         label = "Refuel Threshold",
-        hover = "Refuel when fuel drops below this percentage",
+        hover = "Re-fuel when fuel drops below this percentage",
         options = {
-            {description = "20%", data = 0.20},
+            {description = "30%", data = 0.30},
+            {description = "40%", data = 0.40},
             {description = "50%", data = 0.50},
+            {description = "60%", data = 0.60},
             {description = "99%", data = 0.99},
         },
-        default = 0.99,
+        default = 0.40,
     },
     {
         name = "check_interval",
         label = "Check Interval",
         hover = "How often to check fuel level (seconds)",
         options = {
-            {description = "1 second", data = 1},
-            {description = "3 seconds", data = 3},
             {description = "5 seconds", data = 5},
-            {description = "10 seconds", data = 10},
+            {description = "30 seconds", data = 30},
+            {description = "60 seconds", data = 60},
+            {description = "120 seconds", data = 120},
         },
-        default = 5,
+        default = 120,
     },
     {
         name = "show_messages",
         label = "Show Messages",
-        hover = "Display chat messages when refueling",
+        hover = "Player says message when re-fueling",
         options = {
             {description = "Yes", data = true},
             {description = "No", data = false},
