c56646113e67d3dc84307215c6b45a3169a55fe5
Alison Li <alison-li@github.com>
2025-08-10 12:29:35 -0700

    Add clean function for common items

--- custom-functions/modmain.lua
+++ custom-functions/modmain.lua
@@ -14,4 +14,27 @@ clean = function(prefab)
     end
 end
 
+-- Removes all specified common excess items placed on the ground from the world
+cleancom = function()
+    local common_items = {
+        stinger = true,
+        spidergland = true,
+        spiderhat = true,
+        silk = true,
+        mosquitosack = true,
+        twigs = true
+    }
+
+    for k, v in pairs(GLOBAL.Ents) do
+        if common_items[v.prefab] and 
+           v.components and 
+           v.components.inventoryitem and 
+           v.components.inventoryitem.owner == nil then
+            v:Remove()
+        end
+    end
+end
+
 GLOBAL.clean = clean
+GLOBAL.cleancom = cleancom
+GLOBAL.cc = cleancom -- shorter alias
